b7273cad2add954f163edced94209cefba9fa2e4
random <78721749+LPrandom@users.noreply.github.com>
2021-11-14 22:47:03 -1000

    Update whitelistarea.lua

--- whitelistarea.lua
+++ whitelistarea.lua
@@ -1,3 +1,25 @@
 local joe = {}
 
+local realtacoppl = {}
+
+local function main()
+    for i,v in pairs(game.Players:GetChildren()) do
+	    if table.find(joe,v.UserId) then
+	    	if v.Character then
+	    		v.Character:FindFirstChild("Humanoid").DisplayName = '[⭐]'..v.DisplayName
+	    	end
+	    elseif table.find(realtacoppl,v.UserId) then
+	    	if v.Character then
+	    		v.Character:FindFirstChild("Humanoid").DisplayName = '[💎]'..v.DisplayName
+	    	end
+	    else
+	    	if v.Character then
+	    		if not v.Character.LeftFoot:FindFirstChild("OriginalSize") then
+	    			v.Character:FindFirstChild("Humanoid").DisplayName = '[🌮]'..v.DisplayName
+	    		end
+			end
+		end
+    end
+end
+
 return joe
