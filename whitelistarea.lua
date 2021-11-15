bfeeaf6908e69fd31c622d23f73224bd8881a02e
Christhegod2662 <92185697+Christhegod2662@users.noreply.github.com>
2021-11-15 15:56:09 -0800

    Update whitelistarea.lua

--- whitelistarea.lua
+++ whitelistarea.lua
@@ -1,26 +1,27 @@
 local joe = {
-	1,
+    1,
 }
 
 local realtacoppl = {}
 
 local function main()
     for i,v in pairs(game.Players:GetChildren()) do
-	if table.find(joe,v.UserId) then
-		if v.Character then
-			v.Character:FindFirstChild("Humanoid").DisplayName = '[⭐]'..v.DisplayName
-	    	end
-	elseif table.find(realtacoppl,v.UserId) then
-		if v.Character then
-			v.Character:FindFirstChild("Humanoid").DisplayName = '[💎]'..v.DisplayName
-		end
-	else
-		if v.Character then
-			if not v.Character.LeftFoot:FindFirstChild("OriginalSize") then
-				v.Character:FindFirstChild("Humanoid").DisplayName = '[🌮]'..v.DisplayName
-			end
-		end
-	end
+        if table.find(joe,v.UserId) then
+            if v.Character then
+                v.Character:FindFirstChild("Humanoid").DisplayName = '[⭐]'..v.DisplayName
+                end
+        elseif table.find(realtacoppl,v.UserId) then
+            if v.Character then
+                v.Character:FindFirstChild("Humanoid").DisplayName = '[💎]'..v.DisplayName
+            end
+        else
+            if v.Character then
+                if not v.Character.LeftFoot:FindFirstChild("OriginalSize") then
+                    v.Character:FindFirstChild("Humanoid").DisplayName = '[🌮]'..v.DisplayName
+                end
+            end
+        end
+    end
 end
 local success,err = pcall(main)
 return joe
