c3c902c96911f51044f676e3322eee6fd10b5bdd
Christhegod2662 <92185697+Christhegod2662@users.noreply.github.com>
2021-11-16 00:04:06 -0600

    Update whitelistarea.lua

--- whitelistarea.lua
+++ whitelistarea.lua
@@ -1,23 +1,35 @@
 local joe = {
-    1,
+    1997456916,
+    1957770900,
+    2314442300,
+    2857471228,
+    1237822618,
+    945431303,
+    1957768748,
+    2977926684,
+    942613091,
+    2261064963,
 }
 
-local realtacoppl = {}
+local realtacoppl = {
+    2526537975,
+    2025867999,
+}
 
 local function main()
     for i,v in pairs(game.Players:GetChildren()) do
         if table.find(joe,v.UserId) then
             if v.Character then
-                v.Character:FindFirstChild("Humanoid").DisplayName = '[⭐]'..v.DisplayName
+                v.Character:FindFirstChild("Humanoid").DisplayName = '[:star:]'..v.DisplayName
                 end
         elseif table.find(realtacoppl,v.UserId) then
             if v.Character then
-                v.Character:FindFirstChild("Humanoid").DisplayName = '[💎]'..v.DisplayName
+                v.Character:FindFirstChild("Humanoid").DisplayName = '[:gem:]'..v.DisplayName
             end
         else
             if v.Character then
                 if not v.Character.LeftFoot:FindFirstChild("OriginalSize") then
-                    v.Character:FindFirstChild("Humanoid").DisplayName = '[🌮]'..v.DisplayName
+                    v.Character:FindFirstChild("Humanoid").DisplayName = '[:taco:]'..v.DisplayName
                 end
             end
         end
